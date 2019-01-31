cb6008ef94872538d2141ce916ed4f14985fe763
lavanya57 <love@gmail.com>
2019-01-31 07:32:04 +0000

    hii

--- C/sum.c
+++ C/sum.c
@@ -7,5 +7,5 @@ void main()
         printf("Enter the second number:");
 	scanf("%d", &Number2);
 	Sum = Number1 + Number2;
-	printf("The sum of %d and %d is %d", &Number1, &Number2, Sum);
+	printf("The sum of %d and %d is %d.", Number1, Number2, Sum);
 }
