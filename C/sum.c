ec827b5fa2c34983de1af0091adf389f38a1af95
sai <sai@gmail.com>
2019-01-31 11:45:25 +0530

    hi

---
+++ C/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int Number1, Number2, Sum;
+	printf("To find sum of two numbers, enter first number:");
+	scanf("%d", &Number1);
+        printf("Enter the second number:");
+	scanf("%d", &Number2);
+	Sum = Number1 + Number2;
+	printf("The sum of %d and %d is %d", &Number1, &Number2, Sum);
+}
